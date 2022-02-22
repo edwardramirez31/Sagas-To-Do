bd711798dd2672a40d95639795086aa7a394fd4e
Edward Ramírez <edal_ramirez@hotmail.com>
2022-02-21 23:49:00 -0500

    fix: final instructions for semantic release

--- src/index.js
+++ src/index.js
@@ -14,3 +14,12 @@ ReactDOM.render(
 
 // https://github.com/conventional-changelog/commitlint#getting-started
 // https://typicode.github.io/husky/#/?id=create-a-hook
+// yarn add -D husky
+// yarn husky install
+// yarn husky add .husky/pre-commit "yarn lint-staged"
+// yarn husky add .husky/pre-commit "yarn test"
+// npm install --save-dev @commitlint/config-conventional @commitlint/cli
+// yarn husky add .husky/commit-msg 'npx --no-install commitlint --edit ""'
+// yarn add eslint
+// yarn add lint-staged
+// yarn eslint --init
